0c1a78578d580723547a761aa7e190ed91edf9b2
TheSPD <shreepad007@gmail.com>
2016-11-20 22:48:54 -0500

    Solving linear program with Triangle algorithm

---
+++ linear_programming.py
@@ -0,0 +1,74 @@
+import triangle_algo as triAlg
+import numpy as np
+
+try:
+    print 'All the parameters are for a standard form minimization problem'
+    n = int(raw_input("Enter the number of unknowns : "))
+    m = int(raw_input("Enter number of constraints : "))
+
+
+    print "Enter c (separated by spaces) : "
+
+    c = [map(int, raw_input().split(' '))]
+
+    if n != len(c[0]):
+        raise Exception("Please provide valid input for c")
+
+    print "Enter A (in row major format separated by spaces and new line) : "
+    A = [map(int, raw_input().split(' ')) for _ in range(m)]
+
+    if any([False if len(row) == n else True for row in A]):
+        raise Exception("Please provide valid input for A")        
+
+    print "Enter b (separated by spaces) : "
+
+    b = [map(int, raw_input().split(' '))]
+
+    if m != len(b[0]):
+        raise Exception("Please provide valid input for b")
+
+    print 'Processing...'
+
+    c = np.array(c, np.float64).T
+    A = np.array(A, np.float64)
+    b = np.array(b, np.float64).T
+
+    # m = 2
+    # n = 2
+    # c = np.array([[1, 2]]).T
+    # A = np.array([[1, 2],[3, 4]])
+    # b = np.array([[5, 6]]).T
+
+    zeroMM = np.zeros((m, m))
+    zeroNN = np. zeros((n, n))
+    zeroMN = np.zeros((m, n))
+    zeroNM = np. zeros((n, m))
+
+    negIdenN = -np.identity(n)
+    negIdenM = -np.identity(m)
+
+    
+    A_prime = np.concatenate((-A, zeroMM), axis=1)
+    A_prime = np.concatenate((A_prime, np.concatenate((zeroNN, A.T), axis=1)))
+    A_prime = np.concatenate((A_prime, np.concatenate((c.T, -b.T), axis=1)))
+    A_prime = np.concatenate((A_prime, np.concatenate((-c.T, b.T), axis=1)))
+    A_prime = np.concatenate((A_prime, np.concatenate((negIdenN, zeroNM), axis=1)))
+    A_prime = np.concatenate((A_prime, np.concatenate((zeroMN, negIdenM), axis=1)))
+
+    b_prime = np.concatenate((-b, c))
+    b_prime = np.concatenate((b_prime, np.zeros((2+m+n, 1))))
+
+    epsilon = 0.1
+    B = np.concatenate((A_prime.T, np.zeros((m+n, 1))), axis=1)
+    B = np.concatenate((B, np.concatenate((b_prime.T+epsilon, np.ones((1,1))), axis = 1)))
+
+    p = np.zeros((m+n+1,1))
+    insideOrNot , result, counter = triAlg.triangle_algo(B.T.tolist(), p.T.tolist()[0], 0.001, len(p.T.tolist()[0]))
+    y = [ -1*result[i]/result[len(result)-1] for i in range(len(result))]
+    print insideOrNot , result, counter, y
+    print 'Unknowns are : ', y[:n]
+
+except Exception as e:
+    print e
+    exit()
+
--- triangle_algo.py
+++ triangle_algo.py
@@ -1,19 +1,19 @@
-import numpy as np
+# import numpy as np
 import math
 
 
 ##
-#distance
-#Calculates the distance between two vectors of dimension n
-#Could also go for np.linalg.norm(v1-v2) but used this to optimize the process later
-#@params
-#n - Dimensions of the vector
-#v1 - Vector 1
-#v2 - Vector 2
+# distance
+# Calculates the distance between two vectors of dimension n
+# Could also go for np.linalg.norm(v1-v2) but used this to optimize the process later
+# @params
+# n - Dimensions of the vector
+# v1 - Vector 1
+# v2 - Vector 2
 ##
 def distance(n,v1,v2):
 	if len(v1) != len(v2):
-		return null
+		return None
 	else:
 		distance = 0
 		for i in range(n):
@@ -23,7 +23,7 @@ def distance(n,v1,v2):
 
 def diff(n,v1,v2):
 	if len(v1) != len(v2):
-		return null
+		return None
 	else:
 		v = []
 		for i in range(n):
@@ -32,7 +32,7 @@ def diff(n,v1,v2):
 
 def inner_prod(n, v1, v2):
 	if len(v1) != len(v2):
-		return null
+		return None
 	else:
 		prod = 0
 		for i in range(n):
@@ -46,17 +46,19 @@ def triangle_algo(S, p , epsilon, dim):
 	p_prime = S[prime_index][:]
 	vertex = p_prime[:]
 	alphas = [(1 if prime_index == v else 0 ) for v in range(len(S)) ]
-	print vertex
+	counter = 0
+	# print vertex
 
 	while(True):
+		counter += 1
 		#Step 1 : Check if this p_prime is a epsilon-approximate solution or a witness
-		print "Distance difference : ", distance(dim, p, p_prime) - epsilon * distance(dim, p, vertex)
+		# print "Distance difference : ", distance(dim, p, p_prime) - epsilon * distance(dim, p, vertex)
 		if distance(dim, p, p_prime) < epsilon * distance(dim, p, vertex):
-			return True, alphas
+			return True, alphas, counter
 		else:
 			pivots = [v for v in S if (distance(dim,p_prime,v) > distance(dim,p,v))]
 			if len(pivots) == 0:
-				return False, p_prime
+				return False, p_prime, counter
 			else:
 				vertex = pivots[0]
 				vertex_index = S.index(vertex)
@@ -71,7 +73,7 @@ def triangle_algo(S, p , epsilon, dim):
 				p_prime.append(0)
 				for j in range(len(S)):
 					p_prime[i] += alphas[j] * S[j][i]
-			print p_prime
+			# print p_prime
 
 # v1 = [1,1]
 # v2 = [4,1]
@@ -79,30 +81,53 @@ def triangle_algo(S, p , epsilon, dim):
 
 # S = [v1,v2,v3]
 
-#inside the triangle
+# inside the triangle
 # p = [3,2]
 
-#outside the triangle
+# outside the triangle
 # p = [4,2]
 # 
 
-v1 = [0,0,0]
-v2 = [0,4,0]
-v3 = [4,0,0]
-v4 = [4,4,0]
-v5 = [0,0,4]
-v6 = [0,4,4]
-v7 = [4,0,4]
-v8 = [4,4,4]
+# v1 = [0,0,0]
+# v2 = [0,4,0]
+# v3 = [4,0,0]
+# v4 = [4,4,0]
+# v5 = [0,0,4]
+# v6 = [0,4,4]
+# v7 = [4,0,4]
+# v8 = [4,4,4]
 
-S = [v1,v2,v3,v4,v5,v6,v7,v8]
+# S = [v1,v2,v3,v4,v5,v6,v7,v8]
 
-p = [5,5,4]
+# p = [5, 3, 4]
+# 
 
-epsilon = 0.01
+'''
+epsilon = 0.05
 
-x,y = triangle_algo(S, p, epsilon, len(p))
-print ( x,y )
+v1 = [-2, -1, 0, 0,               -3 + epsilon]
+v2 = [-1, -2, 0, 0,               -6 + epsilon]
+v3 = [0, 0, 2, 1,                  1 + epsilon]
+v4 = [0, 0, 1, 2,                  2 + epsilon]
+v5 = [1, 2, -3, -6,                0 + epsilon]
+v6 = [-1, -2, 3, 6,                0 + epsilon]
+v7 = [-1, 0 , 0 , 0,               0 + epsilon]
+v8 = [0, -1, 0, 0,                 0 + epsilon]
+v9 = [0, 0, -1, 0,                 0 + epsilon]
+v10 = [0, 0, 0, -1,                0 + epsilon]
+v11 = [0,0,0,0,1]
+
+S = [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11]
+
+p = [0, 0, 0, 0, 0]
+epsilon = 0.001
+
+insideOrNot , result, counter = triangle_algo(S, p, epsilon, len(p))
+print ( insideOrNot, result, counter )
+
+print [ -1*result[i]/result[len(result)-1] for i in range(len(result))]
+'''
+
 # sum(y)
 
 # print v1
@@ -117,4 +142,4 @@ print ( x,y )
 # print v1
 # print v2
 # print v3
-# print (v3-v1)
+# 	print (v3-v1)
