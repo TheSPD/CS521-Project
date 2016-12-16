6bb5969e612f601436d059bbc1ae6eaba4d93eaa
TheSPD <shreepad007@gmail.com>
2016-12-16 02:27:47 -0500

    Added input screen

---
+++ DiagonalScaling.py
@@ -0,0 +1,30 @@
+import Tkinter as Tk
+import createGrid as cG
+import numpy as np
+
+
+def show_entry_fields():
+    Q = np.array([[e1.get(), e2.get()], [e3.get(), e4.get()]],
+                 dtype=np.float64)
+    cG.plotGrid(Q, master)
+# print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
+
+print " llal "
+master = Tk.Tk()
+master.wm_title("Diagonal Scaling")
+Tk.Label(master, text="Enter a 2x2 PSD : ").grid(row=0)
+
+e1 = Tk.Entry(master)
+e2 = Tk.Entry(master)
+e3 = Tk.Entry(master)
+e4 = Tk.Entry(master)
+
+e1.grid(row=1, column=0)
+e2.grid(row=1, column=1)
+e3.grid(row=2, column=0)
+e4.grid(row=2, column=1)
+
+Tk.Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=Tk.W, pady=4)
+Tk.Button(master, text='Show', command=show_entry_fields).grid(row=3, column=1, sticky=Tk.W, pady=4)
+
+Tk.mainloop()
--- createGrid.py
+++ createGrid.py
@@ -4,7 +4,7 @@ import numpy as np
 from time import time
 from potentialReduction import potentialReduction as potRed
 
-N = 256
+N = 512
 
 
 def pixel(image, pos, color):
@@ -18,56 +18,58 @@ def fillGrid(photo, grid, colors):
     """Fill the grid with the colors and grid matrix"""
     for i in range(N-1):
         for j in range(N-1):
-            pixel(photo, (i, j), colors[grid[i][j]])
+            pixel(photo, (i, j), colors[grid[i][j] % len(colors)])
 
-def getIter(i, j, N):
-    Q = np.array([[20, 5], [5, 20]], dtype=np.float64)
-    return potRed(np.array([[float(i) / N], [float(j) / N]], dtype=np.float64), Q, 0.01)[1]
+def getIter(i, j, N, Q):
+    return potRed(np.array([[float(i) / N], [float(j) / N]], dtype=np.float64), Q, 1. / N)[1]
+
+def plotGrid(Q, master):
+    t0 = time()
+    grid = [[getIter(i, j, N, Q) for i in range(1, N)] for j in range(1, N)]
+
+    maxIter = max(grid[i][j] for i in range(N-1) for j in range(N-1))
+
+    grid[0][0] = 0
+
+    print 'Time taken... %f' % (time() - t0)
+
+    colors = {i : (255 - ((255/maxIter) * i), 0, 0) for i in range(maxIter)}
+
+    # grid = [[random.randrange(3) for i in range(N)] for j in range(N)]
+    colors = {
+        0: (255, 255, 255),
+        1: (255, 0, 0),
+        2: (0, 255, 0),
+        3: (0, 0, 255),
+        4: (255, 255, 0),
+        5: (255, 0, 255),
+        6: (0, 255, 255),
+        7: (192, 192, 192),
+        8: (128, 0, 0),
+        9: (0, 128, 0),
+        10: (0, 0, 128),
+        11: (0, 128, 128),
+        12: (128, 0, 128),
+        13: (128, 128, 0),
+        14: (128, 128, 128),
+        15: (192, 0, 0),
+        16: (0, 192, 0),
+        17: (0, 0, 192)
+    }
+
+    root = Tkinter.Toplevel(master)
+    root.wm_title("Grid")
+
+    photo = Tkinter.PhotoImage(width=N, height=N)
+
+    # Q = np.array([[20, 18], [18, 30]], dtype=np.float64)
+    # print grid
+    fillGrid(photo, grid, colors)
 
 
-t0 = time()
-# print getIter(1, 1, N)
+    label = Tkinter.Label(root, image=photo)
+    label.grid()
+    root.mainloop()
 
-grid = [[getIter(i, j, N) for i in range(1, N)] for j in range(1, N)]
-# print len(grid)
-maxIter = max(grid[i][j] for i in range(N-1) for j in range(N-1))
-
-grid[0][0] = 0
-
-print 'Time taken... %f' % (time() - t0)
-
-colors = {i : (255 - ((255/maxIter) * i), 0, 0) for i in range(maxIter)}
-
-# grid = [[random.randrange(3) for i in range(N)] for j in range(N)]
-colors = {
-    0: (255, 255, 255),
-    1: (255, 0, 0),
-    2: (0, 255, 0),
-    3: (0, 0, 255),
-    4: (255, 255, 0),
-    5: (255, 0, 255),
-    6: (0, 255, 255),
-    7: (192, 192, 192),
-    8: (128, 0, 0),
-    9: (0, 128, 0),
-    10: (0, 0, 128),
-    11: (0, 128, 128),
-    12: (128, 0, 128),
-    13: (128, 128, 0),
-    14: (128, 128, 128),
-    15: (192, 0, 0),
-    16: (0, 192, 0),
-    17: (0, 0, 192)
-}
-
-root = Tkinter.Tk()
-
-photo = Tkinter.PhotoImage(width=N, height=N)
-
-# print grid
-fillGrid(photo, grid, colors)
-
-
-label = Tkinter.Label(root, image=photo)
-label.grid()
-root.mainloop()
+# Q = np.array([[20, 18], [18, 30]], dtype=np.float64)
+# plotGrid(Q)
